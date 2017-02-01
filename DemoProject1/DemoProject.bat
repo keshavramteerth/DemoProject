c62b2abf1639a73b22e3de6842885b416bf9f153
keshavramteerth <keshavramteerth@gmail.cmo>
2017-02-01 19:34:08 +0530

    updated bat file

--- DemoProject1/DemoProject.bat
+++ DemoProject1/DemoProject.bat
@@ -1,4 +1,4 @@
-set projectLocation=F:\WebDriverwork\DemoProject
+set projectLocation=F:\WebDriverwork\DemoProject1
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
